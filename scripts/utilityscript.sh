6b3ed45274266fe36908f66f4825b4b1ffebc009
Ganesh Parmar <ganeshparmar221997@gmail.com>
2025-12-24 13:13:28 +0530

    updated utility script

--- scripts/utilityscript.sh
+++ scripts/utilityscript.sh
@@ -179,6 +179,7 @@ done
 # Use the correct name of your default branch here (e.g., 'master' or 'main')
 # here we have to give main as a branch name
 # git reset --hard "master" > /dev/null 2>&1
+git reset --hard HEAD
 git checkout master --force 
 
 
